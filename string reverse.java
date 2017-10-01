e77eb006d4e6cee32a95eb9649c1a1ba1bc04617
YOGISHOBAN <yogishoban1996@gmail.com>
2017-10-01 12:03:25 +0530

    Add files via upload

---
+++ string reverse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class good {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Scanner in=new Scanner(System.in);
+ String a=in.next();
+ StringBuffer s=new StringBuffer(a);
+ System.out.println(s.reverse());
+	}
+
+}
